6cb9afdd553dcef27b5b42c67fb8fa5674cb124a
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:43:07 -0300

    Add files via upload

---
+++ EX-9-LISTA-1-Yohan_Aquino/EX-9-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_9_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*10. Calcular e exibir a área de um retângulo, a partir dos valores da base e altura que serão
+digitados. Se a área for maior que 100, exibir a mensagem “Terreno grande”, caso contrário, exibir
+a mensagem “Terreno pequeno”. */
+            Retangulo ret = new Retangulo();
+
+            Console.Write("Informe a base do retângulo: ");
+            ret.setBase(double.Parse(Console.ReadLine()));
+
+            Console.Write("Informe a altura do retângulo: ");
+            ret.setAltura(double.Parse(Console.ReadLine()));
+
+            ret.calcularArea();
+
+            Console.WriteLine("O retângulo tem uma área igual a {0} e pelo padrão um {1} .", ret.getArea(), ret.getTamanho());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EX-9-LISTA-1-Yohan_Aquino/EX-9-LISTA-1-Yohan_Aquino/Retangulo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_9_LISTA_1_Yohan_Aquino
+{
+    class Retangulo
+    {
+        private double b;
+        private double h;
+        private double a;
+
+        public void setBase(double x)
+        {
+            b = x;
+        }
+
+        public void setAltura(double x)
+        {
+            h = x;
+        }
+        public void calcularArea()
+        {
+            a = b * h;
+        }
+        public double getArea()
+        {
+            return a;
+        }
+
+        public string getTamanho() 
+        {
+            if (a>100) 
+            {
+                return "terreno grande";
+            }
+            else 
+            {
+                return "terreno pequeno";
+            }
+        }
+    }
+}
